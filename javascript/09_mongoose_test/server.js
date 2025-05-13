09ce6496a3d18bbe91201f51868bc17753892ea7
Erno Hentonen <erno.hentonen@opiframe.com>
2025-05-13 11:50:51 +0300

    library

---
+++ javascript/09_mongoose_test/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const mongo_url = process.env.MONGODB_URL;
+const mongo_user = process.env.MONGODB_USER;
+const mongo_password = process.env.MONGODB_PASSWORD;
+
+const url = "mongodb+srv://"+mongo_user+":"+mongo_password+"@"+mongo_url+"/?retryWrites=true&w=majority&appName=testiklusteri";
+
+mongoose.connect(url).then(
+	() => console.log("Connected to Mongodb"),
+	(error) => console.log("Failed to connect to Mongodb. Reason",error)
+)
+
+app.listen(3000);
+
+console.log("Running in port 3000");

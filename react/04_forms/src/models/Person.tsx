b4b737fcf48a57e9c328988c42465047a9b7f950
Erno Hentonen <erno.hentonen@opiframe.com>
2025-07-01 11:02:37 +0300

    react 2

---
+++ react/04_forms/src/models/Person.tsx
@@ -0,0 +1,11 @@
+export default class Person {
+	
+	public firstname:string;
+	public lastname:string;
+	
+	constructor(firstname:string,lastname:string) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+	}
+}
+

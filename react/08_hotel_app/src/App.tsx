cad4fe4f050c287023081e7ce8a54cafd52e766f
Erno Hentonen <erno.hentonen@opiframe.com>
2025-07-02 14:39:46 +0300

    hotel app

--- react/08_hotel_app/src/App.tsx
+++ react/08_hotel_app/src/App.tsx
@@ -1,4 +1,8 @@
 import useAction from './hooks/useAction';
+import HotelForm from './components/HotelForm';
+import HotelList from './components/HotelList';
+import Navbar from './components/Navbar';
+import {Route,Routes,Navigate} from 'react-router-dom';
 
 function App() {
   
@@ -6,7 +10,13 @@ function App() {
 
 	return (
 		<>
-      
+			<Navbar/>
+			<hr/>
+			<Routes>
+				<Route path="/" element={<HotelList list={list} removeHotel={removeHotel}/>}/>
+				<Route path="/form" element={<HotelForm addHotel={addHotel}/>}/>
+				<Route path="*" element={<Navigate to="/"/>}/>
+			</Routes>
 		</>
 	)
 }
--- react/08_hotel_app/src/models/Hotel.tsx
+++ react/08_hotel_app/src/models/Hotel.tsx
@@ -1,14 +1,14 @@
 export default class Hotel {
 
-	public name:string;
+	public hname:string;
 	public address:string;
 	public city:string;
 	public stars:number;
 	public roomprice:number;
 	public id:number;
 
-	constructor(name:string,address:string,city:string,stars:number,roomprice:number,id:number) {
-		this.name = name;
+	constructor(hname:string,address:string,city:string,stars:number,roomprice:number,id:number) {
+		this.hname = hname;
 		this.address = address;
 		this.city = city;
 		this.stars = stars;

b4a391ed480b9a29cc1996f5126b46ff4d22138b
Erno Hentonen <erno.hentonen@opiframe.com>
2025-07-08 09:47:56 +0300

    shopping app

--- react/50_shopping_app/01_stage/frontend/src/App.tsx
+++ react/50_shopping_app/01_stage/frontend/src/App.tsx
@@ -1,11 +1,13 @@
-
+import useAction from './hooks/useAction';
+import ShoppingForm from './components/ShoppingForm';
 
 function App() {
 
-
+	const {add} = useAction();
+	
 	return (
 		<>
-
+			<ShoppingForm add={add}/>
 		</>
 	)
 }

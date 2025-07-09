a21a92fccea4d6df0aaec4a6472ab5690e5301bd
Erno Hentonen <erno.hentonen@opiframe.com>
2025-07-09 11:04:03 +0300

    login page

--- react/50_shopping_app/02_stage_login/frontend/src/components/Navbar.tsx
+++ react/50_shopping_app/02_stage_login/frontend/src/components/Navbar.tsx
@@ -1,17 +1,58 @@
 import {List,ListItem} from '@mui/material';
 import {Link} from 'react-router-dom';
 
-const Navbar = () => {
-	return(
-		<List sx={{display:"flex",flexDirection:"row"}}>
-			<ListItem>
-				<Link to="/">Shopping List</Link>
-			</ListItem>
-			<ListItem>
-				<Link to="/form">Add new item</Link>
-			</ListItem>
-		</List>
-	)
+interface Props {
+	isLogged:boolean;
+	loading:boolean;
+	error:string;
+	user:string;
+	logout():void;
+}
+
+const Navbar = (props:Props) => {
+	
+	let message = " ";
+	if(props.loading) {
+		message = "Loading..."
+	}
+	if(props.error) {
+		message = props.error;
+	}
+	if(props.isLogged) {
+		return(
+			<List sx={{display:"flex",flexDirection:"row"}}>
+				<ListItem>
+					Shopping App
+				</ListItem>
+				<ListItem>
+					<Link to="/">Shopping List</Link>
+				</ListItem>
+				<ListItem>
+					<Link to="/form">Add new item</Link>
+				</ListItem>
+				<ListItem>
+					Logged in as {props.user}
+				</ListItem>
+				<ListItem>
+					<Link to="/" onClick={props.logout}>Logout</Link>
+				</ListItem>
+				<ListItem>
+					{message}
+				</ListItem>
+			</List>
+		)
+	} else {
+		return(
+			<List sx={{display:"flex",flexDirection:"row"}}>
+				<ListItem>
+					Shopping App
+				</ListItem>
+				<ListItem>
+					{message}
+				</ListItem>
+			</List>
+		)		
+	}
 }
 
 export default Navbar;

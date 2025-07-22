86414e71cb8c3337b69aa39e6c838f814aae283f
Erno Hentonen <erno.hentonen@opiframe.com>
2025-07-22 12:50:30 +0300

    shopping app context

---
+++ react/50_shopping_app/04_stage_statemanager/frontend/src/context/AppStateContext.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import type {AppState} from '../types/AppState';
+
+
+const AppStateContext = React.createContext<AppState>({
+	list:[],
+	isLogged:false,
+	token:"",
+	loading:false,
+	error:"",
+	user:""
+})
+
+AppStateContext.displayName = "AppStateContext";
+
+export default AppStateContext;

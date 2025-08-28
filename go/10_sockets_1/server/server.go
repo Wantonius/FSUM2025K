43b328f503db4ad212ae44f790afc054635e9424
Erno Hentonen <erno.hentonen@opiframe.com>
2025-08-28 10:59:18 +0300

    go web

---
+++ go/10_sockets_1/server/server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"bufio"
+	"os"
+)
+
+func main() {
+	
+	fmt.Println("Start server at port 5000")
+	
+	in,_ := net.Listen("tcp",":5000")
+	fmt.Println("Waiting for connections")
+	conn,_ := in.Accept()
+
+	message,_ := bufio.NewReader(conn).ReadString('\n')
+	fmt.Println("Message from client:",message)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Message to client:")
+	text,_ := reader.ReadString('\n')
+	fmt.Fprintf(conn,text+"\n")
+}
---
+++ go/11_sockets_2/client/client.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"bufio"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	conn,err := net.Dial("tcp","localhost:5000")
+	if err != nil {
+		fmt.Println("Failed to connect to server. Error:",err.Error())
+		return
+	}
+	
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("Text to send (quit quits)")
+		input,_ := reader.ReadString('\n')
+		conn.Write([]byte(input))
+		fmt.Printf("Message sent %s\n",input)
+		if strings.TrimRight(input,"\r\n") == "quit" {
+			conn.Close()
+			return
+		}
+	}
+}
